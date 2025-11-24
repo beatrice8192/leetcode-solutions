fb0a646b13d86269237b5785ba2e429aeecf25fe
Beatrice <tanyuqing10@gmail.com>
2025-11-23 20:51:54 -0500

    Update

--- 0001-1000-easy-python/0929-unique-email-addresses.py
+++ 0001-1000-easy-python/0929-unique-email-addresses.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/unique-email-addresses
+class Solution(object):
+    # def numUniqueEmails(self, emails: List[str]) -> int:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        _set = set()
+        for email in emails:
+            _email = email.split("@")
+            _set.add(_email[0].split("+")[0].replace(".", "") + "@" + _email[1])
+        return len(_set)
+
