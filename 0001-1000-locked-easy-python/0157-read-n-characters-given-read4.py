7756b18423f0459222b003dd08b88507ed506428
Beatrice <tanyuqing10@gmail.com>
2025-11-27 20:34:16 -0500

    Update

--- 0001-1000-locked-easy-python/0157-read-n-characters-given-read4.py
+++ 0001-1000-locked-easy-python/0157-read-n-characters-given-read4.py
@@ -1,1 +1,33 @@
 # https://leetcode.com/problems/read-n-characters-given-read4
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        i = 0
+        res = -1
+        buf4 = [' '] * 4
+        while (i < n and res != 0):
+            res = read4(buf4)
+            buf[i:i+res] = buf4
+            i += res
+        del buf[n:]
+        return i
+
--- 0001-1000-locked-easy-python/0163-missing-ranges.py
+++ 0001-1000-locked-easy-python/0163-missing-ranges.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/missing-ranges
+class Solution:
+    def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
+        result = []
+        start = lower
+        for n in nums:
+            if (start <= n):
+                if (start < n):
+                    result.append([start, n - 1])
+                start = n + 1
+        if (start <= upper):
+            result.append([start, upper])
+        return result
+
