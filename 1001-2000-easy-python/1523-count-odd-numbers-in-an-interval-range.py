4e2ebb754c79812abad5a8d35080568cab55e006
Beatrice <tanyuqing10@gmail.com>
2025-12-07 13:44:07 -0500

    Update

--- 1001-2000-easy-python/1523-count-odd-numbers-in-an-interval-range.py
+++ 1001-2000-easy-python/1523-count-odd-numbers-in-an-interval-range.py
@@ -1,1 +1,5 @@
 # https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return int((high + 1) / 2) - int(low / 2)
+
--- 1001-2000-easy-python/1925-count-square-sum-triples.py
+++ 1001-2000-easy-python/1925-count-square-sum-triples.py
@@ -1,1 +1,11 @@
 # https://leetcode.com/problems/count-square-sum-triples
+class Solution:
+    def countTriples(self, n: int) -> int:
+        square_set = set([i * i for i in range(1, n + 1)])
+        result = 0
+        for i in range(1, n + 1):
+            for j in range(1, i):
+                if ((i * i + j * j) in square_set):
+                    result += 2
+        return result
+
